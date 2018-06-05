3dc7131cb04ce719b57d7f99e94123136e3f4066
Unknown <almabekov@gmail.com>
2018-06-05 14:21:02 -0300

    DEVELOPMENT, Andrey Almabekov, Nick Meade, user story 1
    Created basic layout, made navigation from main screen to the course description, navigation to the registration activity,

--- app/src/main/java/com/example/achristians/gpproject/MainActivity.java
+++ app/src/main/java/com/example/achristians/gpproject/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.achristians.gpproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 // assumes user open to availibility of course with list view
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNavigateCourseDescription;
+    public static String courseIDstring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.menu);
+
+        courseIDstring="CourseID";
+
+
+        btnNavigateCourseDescription=findViewById(R.id.navigateCourseDescription);
+
+        btnNavigateCourseDescription.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),courseDetails.class);
+                intent.putExtra(courseIDstring,"CSCI3130");
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/example/achristians/gpproject/courseDetails.java
+++ app/src/main/java/com/example/achristians/gpproject/courseDetails.java
@@ -1,7 +1,11 @@
 package com.example.achristians.gpproject;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by AChristians on 2018-05-14.
@@ -9,9 +13,35 @@ import android.support.v7.app.AppCompatActivity;
 
 public class courseDetails extends AppCompatActivity {
 
+    String courseID="";
+    TextView courseIDview;
+    TextView courseDescriptionView;
+
+    private Button btnRegistration;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.course_details);
+
+        courseIDview = findViewById(R.id.courseID);
+        courseDescriptionView = findViewById(R.id.courseDescription);
+        btnRegistration = findViewById(R.id.registerButton);
+
+
+        Intent intent = getIntent();
+        courseID = intent.getStringExtra(MainActivity.courseIDstring);
+        courseIDview.setText(courseID);
+
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),registration.class);
+                // we need to pass course ID here later
+                startActivity(intent);
+            }
+        });
+
     }
 }
