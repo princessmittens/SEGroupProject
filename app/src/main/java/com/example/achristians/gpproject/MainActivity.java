6dae9eaccbb6f58632cf5ed9960689c8b450155e
Unknown <almabekov@gmail.com>
2018-06-13 13:42:25 -0300

    Merged develop to the course description with working login

--- app/src/main/java/com/example/achristians/gpproject/MainActivity.java
+++ app/src/main/java/com/example/achristians/gpproject/MainActivity.java
@@ -6,18 +6,64 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+
 // assumes user open to availibility of course with list view
 public class MainActivity extends AppCompatActivity {
 
+
     private Button btnNavigateCourseDescription;
     public static String courseIDstring;
+    TextView Vlogpass, Vlogemail;
+    EditText Elogpass, Elogemail;
+    Button createaccount, loginbutton;
+    public FirebaseAuth firebaseAuth;
+    firebase fb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu);
+        setContentView(R.layout.login);
 
         courseIDstring="CourseID";
+
+
+
+        setContentView(R.layout.login);
+
+        Vlogemail = findViewById(R.id.Vlogemail);
+        Vlogpass = findViewById(R.id.Vlogpass);
+        Elogpass = findViewById(R.id.Elogpass);
+        Elogemail = findViewById(R.id.Elogemail);
+
+        createaccount = findViewById(R.id.createaccount);
+        loginbutton = findViewById(R.id.loginbutton);
+        fb = new firebase();
+        firebaseAuth = fb.firebaseInstance();
+
+        createaccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, signUpPage.class);
+                startActivity(i);
+            }
+        });
+
+        loginbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = Elogemail.getText().toString();
+                String pass = Elogpass.getText().toString();
+                fb.signIn(MainActivity.this, email, pass);
+                Intent i = new Intent(getApplicationContext() ,MainActivity.class);
+                startActivity(i);
+            }
+        });
 
 
         btnNavigateCourseDescription=findViewById(R.id.navigateCourseDescription);
@@ -31,6 +77,13 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = firebaseAuth.getCurrentUser();
 
     }
 }
