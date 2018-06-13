c45d190a9bdc43566a33201c50c436eacf2b6573
Unknown <simonphilip42@gmail.com>
2018-06-13 15:30:34 -0300

    Added Pushing of Java Objects SW&JM
    Added functionality template for pushing java objects

--- app/src/main/java/com/example/achristians/gpproject/firebaseDB.java
+++ app/src/main/java/com/example/achristians/gpproject/firebaseDB.java
@@ -1,6 +1,7 @@
 package com.example.achristians.gpproject;
 
 import android.content.Context;
+import android.text.format.Time;
 import android.util.Log;
 
 import com.google.firebase.FirebaseApp;
@@ -9,6 +10,9 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class firebaseDB {
 
@@ -22,10 +26,12 @@ public class firebaseDB {
 
     public static firebaseDB dbInterface;
 
+    //Runs test queries
     public static void testQueries(){
         dbInterface.addTestListener();
         dbInterface.setTestObject();
         dbInterface.getUserSpec();
+        dbInterface.setUserTest();
 
         User test = User.SpecUser;
     }
@@ -36,10 +42,11 @@ public class firebaseDB {
         rootDataReference = rootDataSource.getReference();
     }
 
+    //Adds a listener to a database path, whose result will be stored in result
     public void addObjectListener(String DB_path){
         testDataReference = rootDataReference.child(DB_path);
 
-        testDataReference.addListenerForSingleValueEvent(
+        testDataReference.addValueEventListener(
             new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
@@ -54,6 +61,7 @@ public class firebaseDB {
         );
     }
 
+    //Gets the spec user from the database
     public void getUserSpec(){
         usersDataReference = rootDataReference.child("Users");
         DatabaseReference specUserDR = usersDataReference.child("Spec");
@@ -73,6 +81,21 @@ public class firebaseDB {
         );
     }
 
+    //Sets a test user in the database
+    public void setUserTest(){
+        usersDataReference = rootDataReference.child("Users");
+        DatabaseReference testUserDR = usersDataReference.child("Test");
+
+        HashMap<String, Long> completed = new HashMap<>();
+        completed.put("CRN", (long)1);
+        HashMap<String, Long> registered = new HashMap<>();
+        registered.put("CRN", (long)2);
+
+        User testUser = new User("Test", Calendar.getInstance().getTime().toString(), completed, registered);
+        testUserDR.setValue(testUser);
+    }
+
+    //Adds a listener to the Test object, fetching it once
     public void addTestListener(){
         testDataReference = rootDataReference.child("Test");
 
@@ -91,6 +114,7 @@ public class firebaseDB {
         );
     }
 
+    //Sets a single test data point in the database, no template object
     public void setTestObject(){
         testDataReference = rootDataReference.child("Test");
         testDataReference.setValue("New Value");
