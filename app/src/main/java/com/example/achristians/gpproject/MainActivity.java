1245960126084f86d6c9b19c1764d462d44c5289
princessmittens <an747455@dal.ca>
2018-06-14 12:18:15 -0300

    changes to develop

--- app/src/main/java/com/example/achristians/gpproject/MainActivity.java
+++ app/src/main/java/com/example/achristians/gpproject/MainActivity.java
@@ -1,11 +1,5 @@
 package com.example.achristians.gpproject;
-import android.nfc.Tag;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.TextView;
 
-import com.google.firebase.FirebaseApp;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +14,7 @@ import com.google.firebase.auth.FirebaseUser;
 
 // assumes user open to availibility of course with list view
 public class MainActivity extends AppCompatActivity {
+
     TextView Vlogpass, Vlogemail;
     EditText Elogpass, Elogemail;
     Button createaccount, loginbutton;
@@ -29,9 +24,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-//         firebaseDB.dbInterface = new firebaseDB(this);
-//         firebaseDB.testQueries();
         setContentView(R.layout.login);
 
         Vlogemail = findViewById(R.id.Vlogemail);
@@ -70,5 +62,6 @@ public class MainActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         FirebaseUser currentUser = firebaseAuth.getCurrentUser();
+
     }
 }
