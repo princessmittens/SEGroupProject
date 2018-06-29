1dd48c5d940c6e51eb3f7a0b6dbeef1be3385099
nick <nick-m@hotmail.ca>
2018-06-29 12:54:46 -0300

    REFACTOR (Nick & Matt): Cleaned up courseDetails by removing old code for populating fields. Added padding to courseDetails layout to space fields correctly.

--- app/src/main/java/com/example/achristians/gpproject/courseDetails.java
+++ app/src/main/java/com/example/achristians/gpproject/courseDetails.java
@@ -3,31 +3,21 @@ package com.example.achristians.gpproject;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 /**
  * Displays information on a course.
  */
 public class courseDetails extends AppCompatActivity {
 
-    private String courseID= "";
     private TextView courseNameView;
     private TextView userNameView;
     private TextView courseSemesterView;
     private TextView courseCrossListView;
     private TextView courseDescription;
     private Button btnRegistration;
-    private FirebaseAuth auth;
-    private int id;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,14 +31,10 @@ public class courseDetails extends AppCompatActivity {
         courseSemesterView = findViewById(R.id.courseSemesterView);
         courseCrossListView = findViewById(R.id.courseCrossListView);
         courseDescription = findViewById(R.id.courseDescriptionView);
-
         btnRegistration = findViewById(R.id.registerButton);
         userNameView = findViewById(R.id.userNameView);
 
         userNameView.setText(User.getCurrent_Identifier());
-
-        courseID = intent.getStringExtra(Menu.courseIDstring);
-        id = intent.getIntExtra("id", 0);
 
         btnRegistration.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,27 +63,5 @@ public class courseDetails extends AppCompatActivity {
         }
 
         setTitle(c.Course_Code);
-    }
-
-
-    /**
-     * Populates the course information fields.
-     * @param id The id of the course.
-     */
-    private void populateCourseInformation(final int id) {
-        FirebaseDatabase db = FirebaseDatabase.getInstance();
-        DatabaseReference ref = db.getReference().child("Courses/" + id);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Course c = dataSnapshot.getValue(Course.class);
-                populateFromCourse(c);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Log.i("FB", "Listener cancelled for Courses");
-            }
-        });
     }
 }
