abd97dbd5f2eee65939980ddaa21d063ca3f964d
Jake Moore <jake.moorevr@gmail.com>
2018-07-08 17:19:07 -0300

    Added logout and menu tests.

---
+++ app/src/androidTest/java/com/example/achristians/gpproject/LogoutTest.java
@@ -0,0 +1,46 @@
+package com.example.achristians.gpproject;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu;
+import static android.support.test.espresso.Espresso.openContextualActionModeOverflowMenu;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class LogoutTest {
+
+    @Rule
+    public ActivityTestRule<Menu> testRule = new ActivityTestRule<>(Menu.class);
+
+    //Test 1: check if the menu exists
+    @Test
+    public void menuExists() {
+        openContextualActionModeOverflowMenu();
+        onView(withText("Logout")).check(matches(isDisplayed()));
+    }
+
+    //Test 2: Click logout button
+    @Test
+    public void clickLogout() {
+        openContextualActionModeOverflowMenu();
+        onView(withText("Logout")).perform(click());
+    }
+}
---
+++ app/src/androidTest/java/com/example/achristians/gpproject/MenuTest.java
@@ -0,0 +1,59 @@
+package com.example.achristians.gpproject;
+
+import android.support.test.espresso.Espresso;
+import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onData;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.Espresso.pressBack;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.anything;
+
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MenuTest {
+
+    @Rule
+    public ActivityTestRule<Menu> testRule = new ActivityTestRule<>(Menu.class);
+    //Test 1: check if the menu exists
+    @Test
+    public void menuExists() {
+        onView(withId(R.id.navigateCourseDescription)).check(matches(isDisplayed()));
+        onView(withId(R.id.navListView)).check(matches(isDisplayed()));
+    }
+
+    //Test 2: Click menu button navListView
+    @Test
+    public void clickCourseDescription() {
+        onView(withText("Course Description")).perform(click());
+        pressBack();
+    }
+
+    //Test 3: Click menu button navigateCourseDescription
+    @Test
+    public void clickCourseList() {
+        onView(withText("Course List")).perform(click());
+        pressBack();
+    }
+
+    //Test 4: Both buttons
+    @Test
+    public void clickBothButtons() {
+        onView(withText("Course Description")).perform(click());
+        pressBack();
+        onView(withText("Course List")).perform(click());
+        pressBack();
+    }
+}
