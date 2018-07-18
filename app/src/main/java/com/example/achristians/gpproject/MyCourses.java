52ed21c8406a6dd6be7423f13bfb52b84d0e13d5
Marlee Donnelly <mdonnelly@dal.ca>
2018-07-18 14:09:48 -0300

    Technically works but gross
    Displays list of courses that the user is registered for, but only if they have registered in the current app session.
    
    Wonky stuff seems to be happening with the database and this will require investigation.
    
    MD + NM

--- app/src/main/java/com/example/achristians/gpproject/MyCourses.java
+++ app/src/main/java/com/example/achristians/gpproject/MyCourses.java
@@ -1,49 +1,59 @@
 package com.example.achristians.gpproject;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.ArrayList;
+/**
+ * Created by AChristians on 2018-05-14.
+ */
 
-public class MyCourses extends AppCompatActivity {
-    ArrayList<Course> userCourses = new ArrayList<>();
+public class MyCourses extends Menu {
+
+    //Full list of courses from DB
+    ArrayList<Course> courseList = new ArrayList<>();
     ArrayList<Listing> listingList = new ArrayList<>();
     ArrayAdapter<Course> arrayAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.my_courses);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        final ListView courseListView = findViewById(R.id.myCourseListView);
 
-        final ListView listView = findViewById(R.id.myCourseListView);
         Database.dbInterface = new Database(getApplicationContext());
-        fetchUserCourses();
+        fetchCourses();
 
         //Add data to courseListView
-        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, userCourses);
-        listView.setAdapter(arrayAdapter);
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, courseList);
+        courseListView.setAdapter(arrayAdapter);
 
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        courseListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(MyCourses.this, courseDetails.class);
 
-                Course clicked = userCourses.get(position);
+                Course clicked = courseList.get(position);
                 ArrayList<Listing> availableListings = new ArrayList<Listing>();
 
-                for (Listing L : listingList) {
-                    if (L.Key.equals(clicked.Key)) {
+                for(Listing L : listingList){
+                    if(L.Key.equals(clicked.Key)){
                         availableListings.add(L);
                     }
                 }
@@ -56,9 +66,9 @@ public class MyCourses extends AppCompatActivity {
     }
 
     /**
-     * Fetches list of courses that the logged in user has registered for
+     * Fetches course information from backing db on startup, no filtering/searching
      */
-    public void fetchUserCourses(){
+    public void fetchCourses(){
         DatabaseReference coursesDataReference = Database.rootDataReference.child("Courses/");
 
         coursesDataReference.addValueEventListener(
@@ -67,8 +77,14 @@ public class MyCourses extends AppCompatActivity {
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         ArrayList<Course> inputCourses = new ArrayList<>();
                         Iterable<DataSnapshot> dataSnapshots = dataSnapshot.getChildren();
+                        HashMap<String, String> toShow = User.getUser().getRegistered();
                         for (DataSnapshot dsCourse: dataSnapshots) {
-                            inputCourses.add(dsCourse.getValue(Course.class));
+                            Course currentCourse = dsCourse.getValue(Course.class);
+                            for(HashMap.Entry<String, String> userCourse : toShow.entrySet()){
+                                if(currentCourse.Key.equals(userCourse.getKey())){
+                                    inputCourses.add(currentCourse);
+                                }
+                            }
                         }
                         courseChangeHandler(inputCourses);
                     }
@@ -100,11 +116,17 @@ public class MyCourses extends AppCompatActivity {
         );
     }
 
+    /**
+     * Handles the event of course data being pushed to the application from an external source
+     * (The DB). Extracted from the value event listener so functionality/inputs can be mocked for
+     * testing.
+     * @param courseListNew A new arraylist of courses to display
+     */
     public void courseChangeHandler(ArrayList<Course> courseListNew){
         //Emptying the course list, as anytime data is changed db side this method will
         //be called, and add all elements to the end of the list
-        userCourses.clear();
-        userCourses.addAll(courseListNew);
+        courseList.clear();
+        courseList.addAll(courseListNew);
         arrayAdapter.notifyDataSetChanged();
     }
 }
