38769ea757120434165d22d4d1f2a3b650014c07
nick <nick-m@hotmail.ca>
2018-07-20 11:45:10 -0300

    REFACTORING (Nick & Simon): Added documentation to Course class.

--- app/src/main/java/com/example/achristians/gpproject/Course.java
+++ app/src/main/java/com/example/achristians/gpproject/Course.java
@@ -1,28 +1,31 @@
 package com.example.achristians.gpproject;
 
 import com.google.firebase.database.IgnoreExtraProperties;
-import com.google.firebase.database.PropertyName;
-
 import java.io.Serializable;
 
-//Changing this file so Git will decide that my version is up to date
-
+/** Represents a course in Firebase. */
 @IgnoreExtraProperties
 public class Course implements Serializable{
 
+    /** The course code. */
     public String Course_Code;
+    /** The cross-listings. */
     public String Cross_Listing;
+    /** The cross-listings urls. */
     public String Cross_Listing_URL;
+    /** The course description. */
     public String Description;
     public String Key;
+    /** The name of the course. */
     public String Name;
+    /** The prerequisites of the course. */
     public String Requirements;
+    /** The semester the course is held during. */
     public String Semester;
 
-    public static Course exampleCourse = new Course("MATH 1000", "N/A", "", "It's a course. It's alive.", "MATH 9530 WINTER (2) : 07-JAN-2019 - 08-APR-2019", "Calculus 1", "Winter", "");
-
-
-    public Course() {}
+    public Course() {
+        /* Empty contructor for Firebase. */
+    }
 
     public Course(String course_Code, String cross_Listing, String cross_Listing_URL,
                   String description, String key, String name, String semester, String requirements) {
