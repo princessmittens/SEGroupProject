f7c153ff83513d4a15e1462b086d66222e5b36c2
Unknown <almabekov@gmail.com>
2018-07-23 11:44:39 -0300

    [Andrey, Andrea] timetable with basic info works

--- app/src/main/java/com/example/achristians/gpproject/weeklySchedule.java
+++ app/src/main/java/com/example/achristians/gpproject/weeklySchedule.java
@@ -16,6 +16,9 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 
 /**
  * Created by AChristians on 2018-05-14.
@@ -28,19 +31,29 @@ public class weeklySchedule extends AppCompatActivity {
     ArrayList<LinearLayout> daysColumns = new ArrayList<>();
     LinearLayout daysLayout;
     ArrayList<Listing> listingList = new ArrayList<>();
+    ArrayList<ArrayList<Listing>> listingsByDays = new ArrayList();
+    //Match days letters like M, T, W with the arralist indexes
+    HashMap<Character, Integer> daysAbbr= new HashMap<>();
+    //display width
+    int width;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weekly_sched);
 
+        daysAbbr.put('M',0); //monday
+        daysAbbr.put('T',1); //tuesday
+        daysAbbr.put('W',2); //wednesday
+        daysAbbr.put('R',3); //thursday
+        daysAbbr.put('F',4); //friday
 
         //https://stackoverflow.com/questions/17481341/how-to-get-android-screen-size-programmatically-once-and-for-all
         //to create columns with even size
         Display display = getWindowManager().getDefaultDisplay();
         Point size = new Point();
         display.getSize(size);
-        int width = size.x;
+        width = size.x;
 
         daysLayout = findViewById(R.id.horizontalLayout);
         for (int i = 0;i<days.length;i++) {
@@ -77,8 +90,7 @@ public class weeklySchedule extends AppCompatActivity {
 
 
         fetchListings();
-        Log.d("LISTINGS ADDED: ", String.valueOf(listingList.size()));
-        Log.d("USER HASH MAP SIZE",String.valueOf(User.getUser().getRegistered().size()));
+
     }
 
 
@@ -95,13 +107,20 @@ public class weeklySchedule extends AppCompatActivity {
                             Listing l = dsListing.getValue(Listing.class);
                             //Log.d("LISTING:",String.valueOf(l.CRN));
                             if (User.getUser().getRegistered().containsKey(l.Key) && User.getUser().getRegistered().get(l.Key).equals(String.valueOf(l.CRN))) {
-                                listingList.add(dsListing.getValue(Listing.class));
-                                Log.d("Match found","Match found. add listing to the list");
-                                Log.d("CRN is ", String.valueOf(l.CRN));
+                                listingList.add(l);
+                                //Log.d("RESULT_LOG","Match found. add listing to the list");
+                                //Log.d("CRN is ", String.valueOf(l.CRN));
                             }
-                            if (User.getUser().getRegistered().containsKey(l.Key)) Log.d("FOUND MATCH KEY", l.Key);
+                            //if (User.getUser().getRegistered().containsKey(l.Key)) Log.d("FOUND MATCH KEY", l.Key);
 
                         }
+                        Log.d("RESULT_LOG: ", String.valueOf(listingList.size()));
+                        Log.d("RESULT_LOG: ",String.valueOf(User.getUser().getRegistered().size()));
+
+                        //populate timetable with listings
+                        PopulateTimetable(listingList);
+
+
                     }
 
                     @Override
@@ -112,5 +131,57 @@ public class weeklySchedule extends AppCompatActivity {
         );
 
     }
+
+    void PopulateTimetable(ArrayList<Listing> listingList) {
+
+        //create 5 lists for 5 days
+        for (int i=0;i<5; i++) {
+            listingsByDays.add(new ArrayList<Listing>());
+        }
+
+
+        //put listings to the days array list accourding to the 'Days' key. For example if Days are "TR" we need to put this listing
+        //both in the tuesday list and to the thursday list.
+        for (Listing l: listingList) {
+            String days = l.Days;
+            for (int i = 0; i < days.length(); i++) {
+                int index = daysAbbr.get(days.charAt(i));
+                listingsByDays.get(index).add(l);
+                Log.d("RESULT_LOG: ", "Add listing CRN " + l.CRN + " to the day " + String.valueOf((index)));
+            }
+        }
+
+        //now we need to sort each day by the starting time.
+        //We need Comparator to sort listings using their Listing.Time string field
+        //https://stackoverflow.com/questions/2839137/how-to-use-comparator-in-java-to-sort
+        class TimeComparator implements Comparator<Listing> {
+            @Override
+            public int compare(Listing a, Listing b) {
+                return a.Time.compareToIgnoreCase(b.Time);
+                }
+        }
+
+        for (ArrayList<Listing> day: listingsByDays) {
+            Collections.sort(day, new TimeComparator());
+        }
+
+        //now we need just iterate through all days and add all listings to the corresponding timetable layouts
+        int index=0;
+        for (ArrayList<Listing> day: listingsByDays) {
+            for (Listing l: day) {
+                TextView listingView=new TextView(this);
+                listingView.setWidth(width/days.length);
+                //To make text like "CSCI 1105"
+                listingView.setText(l.Key.substring(0,9));
+                LinearLayout column = daysColumns.get(index);
+                column.addView(listingView);
+            }
+
+            //we need to count index to know to what layout to add listing
+            index++;
+        }
+
+    }
+
 }
 
